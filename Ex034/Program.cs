bf20594eacc38848cca6c6e89f8f9363c34a0e56
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-12-01 14:01:07 +0300

    Ex034

---
+++ Ex034/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 34:
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] getRandomArray(int Length, int min, int max)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int getEvenNumberArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int[] newArray = getRandomArray(6, 100, 1000);
+printArray(newArray);
+Console.ResetColor();
+int count = getEvenNumberArray(newArray);
+Console.WriteLine($"Количество четных чисел в массиве = {count}. ");
+Console.ResetColor();
+
